dee57bbc564f2c6473c63d6b96acc3bca4f11fda
Krishna <krishnaagrawal2992@gmail.com>
2024-11-29 21:12:02 +0530

    main index.js back

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,37 @@
-const express = require("express")
-const cors = require("cors")
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const cors = require('cors')
+require('dotenv').config()
+
+const partnerRoutes = require('../routes/partnerRoutes')
 
 const app = express()
 
+app.use(cors({ origin: '*' }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+mongo_URL = process.env.mongo_URL
+const port = process.env.PORT
+
+mongoose
+    .connect(mongo_URL)
+    .then(() => {
+        console.log("Connected to MongoDB Atlas");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB Atlas:", error);
+    });
+
+app.use('/', partnerRoutes)
+
 app.get("/", (req, res) => {
-    res.send("working")
+    res.send("workinggg")
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`)
 })
 
 module.exports = app
-

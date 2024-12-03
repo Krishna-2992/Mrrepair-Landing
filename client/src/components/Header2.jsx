ad011fc6c27b39a109af2dd44e862f79059d1e16
Krishna <krishnaagrawal2992@gmail.com>
2024-12-03 21:44:22 +0530

    Hero section image

---
+++ client/src/components/Header2.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+export default function Header2() {
+    return (
+        <div className='w-full'>
+            <img src="/Hero.jpg" alt="" className='w-full' />
+        </div>
+    )
+}
+
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,15 +2,16 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import RegistrationForm from "../components/RegistrationForm";
+import Header2 from "../components/Header2";
 
 export default function Landing() {
-    return (
+  return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
-      <Header />
+      <Header2 />
       <RegistrationForm />
       <Footer />
-    </div> 
-    )
+    </div>
+  )
 }
 
